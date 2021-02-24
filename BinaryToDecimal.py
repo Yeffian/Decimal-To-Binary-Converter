3476e4c6fe6a75b0b3133b9709d5c83d25f1930d
Captain Ravioli <48518572+Captain-Ravioli@users.noreply.github.com>
2021-02-24 12:48:48 +0200

    Update BinaryToDecimal.py

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -1,21 +1,25 @@
-# Python program to convert decimal to binary
-# Author: Yeffian the Teapot   
+# Python program to convert decimal to binary (and vice versa)
+# Author: Yeffian the Teapot
 
-num = 0 # Declaring a num variable to check and store input
-exitCode = 1 # Declaring a exit code, which the user can input to stop the program
-
+exitCode = '.' # Declaring a exit code, which the user can input to stop the program
 
 # Function to convert decimal to binary
-def decimalToBinary(n): 
+def decimal_to_binary(n): 
     return bin(n).replace("0b", "") 
-   
 
+# Function to convert binary to decimal
+def binary_to_decimal(n):
+    return int(n, 2)
+   
+print("Welcome to my decimal to/from binary converter! Choose a mode: A: decimal to binary, B: binary to decimal")
 # Infinite loop to check for input
 while True:
-    if num == exitCode:   # Checking if the input is the exit code
-        print("Goodbye!")
-        break # Using the 'break' keyword to break out of the loop
-    # If the input is not the exit code
-    print("Welcome to my decimal to binary converter! Use the exit code '1' to exit the program!")
-    num = int(input("Enter a decimal number: "))  # Asking for input
-    print(decimalToBinary(num))  # Printing the binary output
+    num = input("Enter the mode: ") # Asking for input
+    if num == "A":
+        num = int(input("Enter the decimal number you wish to convert:"))
+        print(f"It's {decimal_to_binary(num)}!") # Printing the binary output
+    elif num == "B":
+        num = input("Enter the binary number you wish to convert:")
+        print(f"It's {binary_to_decimal(num)}!") # Printing the decimal output
+    else:
+        print("Please choose between \"A\" and \"B.\"") # Printing that the mode is invalid
